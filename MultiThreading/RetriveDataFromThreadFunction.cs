9ee50fc7323f493b937b26161331c9f1fafae75b
96068chandra <chandraj592001@gmail.com>
2024-10-09 11:00:36 +0530

    MultiThreading(Passing input to the thread and Retrieve Data from a Thread Function)

---
+++ MultiThreading/RetriveDataFromThreadFunction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public delegate void ResultCallbackDelegate(int result);
+    public class RetriveDataFromThreadFunction
+    {
+        private int _number;
+        private ResultCallbackDelegate _resultCallback;
+
+        public RetriveDataFromThreadFunction(int number,ResultCallbackDelegate resultCallback)
+        {
+            _number = number;
+            _resultCallback = resultCallback;
+            
+        }
+        public void CalculateSum()
+        {
+            int res = 0;
+            for(int i=0;i<5;i++)
+            {
+                res=res+i;
+            }
+            if(_resultCallback != null)
+            {
+                _resultCallback(res);
+            }
+
+
+        }
+
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ResultCallbackDelegate resultCallbackDelegate = new ResultCallbackDelegate(ResultCallBackMethod);
+            int num = 10;
+            RetriveDataFromThreadFunction obj = new RetriveDataFromThreadFunction(10,resultCallbackDelegate);
+            Thread thread = new Thread(new ThreadStart(obj.CalculateSum));
+            thread.Start();
+            Console.ReadLine();
+
+        }
+        public static void ResultCallBackMethod(int Result)
+        {
+            Console.WriteLine("The Result is " + Result);
+        }
+    }
+}
+
---
+++ MultiThreading/Thread_Start.cs
@@ -0,0 +1,63 @@
+﻿class Thread_Start
+{
+    static void Main2(string[] args)
+    {
+        //ThreadStart start = new ThreadStart(Displaynumber);
+        //Thread thread = new Thread(start);
+        //thread.Start();
+        ////Using anonymous method
+        //Thread thread1 = new Thread(delegate ()
+        //{
+        //    for (int i = 0; i < 10; i++) 
+        //    {
+        //        Console.WriteLine("Method 2:" + i);
+        //    }
+
+        //});
+        //thread1.Start();
+        ////Using lamda function
+        //Thread thread2 = new Thread(() =>
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Console.WriteLine("Method 3:" + i);
+        //    }
+
+        //});
+        //thread2.Start();
+        int Max = 10;
+        NumberHelper obj = new NumberHelper(Max);
+
+        Thread T1 = new Thread(new ThreadStart(obj.DisplayNumbers));
+
+        T1.Start();
+        Console.Read();
+        
+            
+    }
+    public static void Displaynumber()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Method 1:"+i);
+        }
+    }
+}
+//This example used to pass the data as input to the thread 
+public class NumberHelper
+{
+    int _Number;
+
+    public NumberHelper(int Number)
+    {
+        _Number = Number;
+    }
+
+    public void DisplayNumbers()
+    {
+        for (int i = 1; i <= _Number; i++)
+        {
+            Console.WriteLine("value : " + i);
+        }
+    }
+}
